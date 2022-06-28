daa5eb22c75c37d704b69b0006d8e2eabcf1f001
DHARMIK  GANGANI <dharmikgangani786@gmail.com>
2022-06-28 19:38:09 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,7 +4,7 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/-iQW3NB5pfRNFsNsoo1Teg05Cv_2WYgz',
+      url: 'YOUR_ALCHEMY_API',
       accounts: ['f04ac45a9d447a11e60480b3bf5c44724c912528e8aebca06fe7d90c3b896f7b'],
     },
   },
