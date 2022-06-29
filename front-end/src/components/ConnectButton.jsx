9759419080d1f69bf5a0e60d37f1483287529814
DHARMIK  GANGANI <dharmikgangani786@gmail.com>
2022-06-29 09:11:33 +0530

    Delete front-end directory

--- front-end/src/components/ConnectButton.jsx
+++
@@ -1,4 +0,0 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-export const ConnectButton = () => {
-  return <ConnectButton />;
-};
--- front-end/src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-import abi from "./Transactions.json";
-
-export const contractAddress = "0xB0D54A173E3221083EB1fAA29F3bC52921E285b0";
-export const contractABI = abi.abi;
